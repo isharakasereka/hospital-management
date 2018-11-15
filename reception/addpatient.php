10832476b7986480148b32e7b781e89b0823b1fa
Ishara Kasereka Ndivito <44477852+isharakasereka@users.noreply.github.com>
2018-11-15 08:19:19 -0500

    Update addpatient.php
    I've tried to updatethe form 'addpatient' by  adding some  options on 'birthyear'

--- reception/addpatient.php
+++ reception/addpatient.php
@@ -43,6 +43,9 @@ if (empty($_SESSION['reception']) OR empty($_SESSION['type'])) {
 
 				<select name="birthyear" class="form" required="required">
 					<option value="">Choose Birth Year</option>
+					<option>2018</option>
+					<option>2017</option>
+					<option>2016</option>
 					<option>2015</option>
 					<option>2014</option>
 					<option>2013</option>
@@ -87,8 +90,65 @@ if (empty($_SESSION['reception']) OR empty($_SESSION['type'])) {
 					<option>1974</option>
 					<option>1973</option>
 					<option>1972</option>
-					<option>1971</option>
 					<option>1970</option>
+					<option>1969</option>
+					<option>1968</option>
+					<option>1967</option>
+					<option>1966</option>
+					<option>1965</option>
+					<option>1964</option>
+					<option>1963</option>
+					<option>1962</option>
+					<option>1961</option>
+					<option>1960</option>
+					<option>1959</option>
+					<option>1958</option>
+					<option>1957</option>
+					<option>1956</option>
+					<option>1958</option>
+					<option>1957</option>
+					<option>1956</option>
+					<option>1955</option>
+					<option>1954</option>
+					<option>1953</option>
+					<option>1952</option>
+					<option>1951</option>
+					<option>1950</option>
+					<option>1949</option>
+					<option>1948</option>
+					<option>1947</option>
+					<option>1946</option>
+					<option>1945</option>
+					<option>1944</option>
+					<option>1943</option>
+					<option>1942</option>
+					<option>1941</option>
+					<option>1940</option>
+					<option>1939</option>
+					<option>1938</option>
+					<option>1937</option>
+					<option>1936</option>
+					<option>1935</option>
+					<option>1934</option>
+					<option>1933</option>
+					<option>1932</option>
+					<option>1931</option>
+					<option>1930</option>
+					<option>1929</option>
+					<option>1928</option>
+					<option>1927</option>
+					<option>1926</option>
+					<option>1925</option>
+					<option>1924</option>
+					<option>1923</option>
+					<option>1922</option>
+					<option>1921</option>
+					<option>1920</option>
+					<option>1919</option>
+					<option>1918</option>
+					<option>1917</option>
+					
+					
 				</select><br><br>
 				<input type="submit" value="Add" class="btnlink" name="btn"><br><br>
 			</form>
